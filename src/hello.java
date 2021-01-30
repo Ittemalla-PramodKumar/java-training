b7f7f375964acc50c737fccdf8d10e1f6efcb457
pramod <pramodtony98@gmail.com>
2021-01-30 17:27:13 +0530

    inital commit

---
+++ src/hello.java
@@ -0,0 +1,14 @@
+public class hello {
+
+    public static void main(String[] args) {
+    System.out.println("Hello , tim ");
+
+    int myFirstNumber = (5 + 10) + (2 * 20 );
+    int mySecondNUmber = 14;
+    int myThirdNumber = 42;
+    int myTotal = myFirstNumber + mySecondNUmber + myThirdNumber;
+        System.out.println(myTotal );
+       int myLastOne = 400 - myTotal;
+        System.out.println(myLastOne);
+    }
+}
